c84f8eb735a3334936ea43d67c8426d59f069046
Emma Humphries <ech@emmah.net>
2016-04-28 16:36:27 -0700

    initial parsing rules

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 console.log('in index.js');
 
-var parse = function() {
-    return 'parsed bug';
+var parse = function(bug) {
+    readable = '';
+
+    if (bug.status) {
+        readable = bug.status.toUpperCase() + ' bug';
+    }
+
+    if (bug.triage && bug.triage.toLowerCase() === 'untriaged') {
+        readable = 'UNTRIAGED ' + readable;
+    }
+
+    if (readable === '') {
+        return { error: 'NO_STRING_FOR_BUG' };
+    }
+
+    return readable;
 }
 
 exports.readable = function(args) {
@@ -12,11 +26,11 @@ exports.readable = function(args) {
 
     if (Array.isArray(args)) {
         var result = { result: [] };
-        args.forEach(function() {
-            result.result.push(parse());
+        args.forEach(function(bug) {
+            result.result.push(parse(bug));
         });
         return result;
     }
 
-    return parse(); 
+    return parse(args); 
 };
--- test/tests.js
+++ test/tests.js
@@ -9,13 +9,31 @@ describe('Signatures', function() {
     });
     describe('Array', function() {
         it('If called with an array, should return an array of results', function() {
-            var result = readable([1, 2, 3]);
+            var result = readable([{}, {}, {}]);
             result.result.length.should.equal(3);
         });
     });
-    describe('String', function() {
+    describe('Object', function() {
         it('If called with something else, should return a string', function() {
-            readable({}).should.equal('parsed bug');
+            readable({status: 'murble'}).should.equal('MURBLE bug');
+        });
+    });
+    describe('Missing', function() {
+        it('If called with no valid bug properties, should return an error', function() {
+            readable({}).should.deep.equal({error: 'NO_STRING_FOR_BUG'});
+        });
+    });
+});
+
+describe('Parsing', function() {
+    describe('Status', function() {
+        it('Should return a sentence based on the bug\'s status', function() {
+            readable({status: 'tweaked'}).should.equal('TWEAKED bug');
+        });
+    });
+    describe('Triage', function() {
+        it('Should start with \'untriaged\' if bug is untriaged', function() {
+            readable({status: 'weird', triage: 'UNTRIAGED'}).should.equal('UNTRIAGED WEIRD bug');
         });
     });
 });
